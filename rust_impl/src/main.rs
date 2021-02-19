436ad23f7fdadb6387c1309a4705d3849a2b7388
Stephen Seo <seo.disparate@gmail.com>
2021-02-19 14:38:52 +0900

    Impl Rust version of solution

--- rust_impl/src/main.rs
+++ rust_impl/src/main.rs
@@ -1,3 +1,22 @@
+use structopt::StructOpt;
+use std::collections::HashMap;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "RunLengthEncoding")]
+struct Opts {
+    input: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opts = Opts::from_args();
+
+    let mut map: HashMap<char, u32> = HashMap::new();
+
+    for c in opts.input.chars() {
+        map.get_mut(&c).map(|v| *v += 1).or_else(|| {map.insert(c, 1); Some(())});
+    }
+
+    for (c, value) in map {
+        println!("{} with value {}", c, value);
+    }
 }
