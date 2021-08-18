0e5f84c3bf5a8547bebeb1d3eaf2c22d529bf7c4
Stephen Seo <seo.disparate@gmail.com>
2021-08-18 20:31:47 +0900

    Fix rust_impl

--- rust_impl/src/main.rs
+++ rust_impl/src/main.rs
@@ -1,5 +1,4 @@
 use structopt::StructOpt;
-use std::collections::HashMap;
 
 #[derive(StructOpt, Debug)]
 #[structopt(name = "RunLengthEncoding")]
@@ -10,13 +9,29 @@ struct Opts {
 fn main() {
     let opts = Opts::from_args();
 
-    let mut map: HashMap<char, u32> = HashMap::new();
-
+    let mut current: Option<char> = None;
+    let mut current_count: u32 = 0;
+    let mut list: Vec<(char, u32)> = Vec::new();
     for c in opts.input.chars() {
-        map.get_mut(&c).map(|v| *v += 1).or_else(|| {map.insert(c, 1); Some(())});
+        if c.is_ascii_whitespace() {
+            continue;
+        } else {
+            if current.is_none() || current.unwrap() != c {
+                if current_count > 0 {
+                    list.push((current.unwrap(), current_count));
+                }
+                current_count = 1;
+                current = Some(c);
+            } else {
+                current_count += 1;
+            }
+        }
+    }
+    if current_count > 0 && current.is_some() { 
+        list.push((current.unwrap(), current_count));
     }
 
-    for (c, value) in map {
-        println!("{} with value {}", c, value);
+    for (c, count) in list {
+        println!("{}: has count of {}", c, count);
     }
 }
